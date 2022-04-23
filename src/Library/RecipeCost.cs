d0eda990155547afe0a09f317005c01da5234259
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-04-23 00:13:40 -0300

    Comentario sobre minutos/horas realizado

--- src/Library/RecipeCost.cs
+++ src/Library/RecipeCost.cs
@@ -1,7 +1,9 @@
 namespace Full_GRASP_And_SOLID.Library
 {
     /// <summary>
-    /// Clase "RecipeCost" la cual se le asignó la responsabilidad de calcular todos los costos para realizar una receta. cumple con el patron SRP ya que su unica función es calcular los costos.
+    /// Clase "RecipeCost" la cual se le asignó la responsabilidad de calcular todos los costos para realizar una receta. cumple con el patron SRP ya que su unica función es calcular los costos y en un futuro si se quiere cambiar la forma de calcular los costos
+    /// o hacer algun otro cambio similar solamente se vería afectada esta clase sin tener que hacer grandes cambios en el resto del programa.
+    /// A la hora de clacular los costos por hora, dividí el tiempo entre 60 ya que asumí que el tiempo que se ingresa es en minutos, (no se podria demorar 120 horas en hacer un café)
     /// </summary>
     public class RecipeCost
     {
